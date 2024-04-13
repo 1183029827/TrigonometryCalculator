d1b380cd9befcbb3b763ef4556224db193931a5b
ahhh1235 <165551170+ahhh1235@users.noreply.github.com>
2024-04-13 16:00:38 +0800

    Update TrigCalculator.py

--- TrigCalculator.py
+++ TrigCalculator.py
@@ -3,18 +3,36 @@ import math
 
 
 def calculate_factorial(n):
-    """计算阶乘， 由宋纯豪同学进行编写"""
-    return 0
+        """计算阶乘"""
+    if n == 0:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
+
+
 
 
 def calculate_sin_taylor(x, terms):
-    """使用泰勒级数展开计算正弦函数， 由宋纯豪同学进行编写"""
-    return 0
+      """使用泰勒级数展开计算正弦函数"""
+    x = math.radians(x)  # 将角度转换为弧度
+    sinx = 0
+    for n in range(terms):
+        numerator = (-1) ** n
+        denominator = calculate_factorial(2 * n + 1)
+        sinx += (numerator * x ** (2 * n + 1)) / denominator
+    return sinx
+  
 
 
 def calculate_cos_taylor(x, terms):
-    """使用泰勒级数展开计算余弦函数， 由宋纯豪同学进行编写"""
-    return 0
+        """使用泰勒级数展开计算余弦函数"""
+    x = math.radians(x)  # 将角度转换为弧度
+    cosx = 0
+    for n in range(terms):
+        numerator = (-1) ** n
+        denominator = calculate_factorial(2 * n)
+        cosx += (numerator * x ** (2 * n)) / denominator
+    return cosx
 
 
 def calculate_sin():
