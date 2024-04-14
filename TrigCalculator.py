5f2c8c95806386a06a355de0a994b53292ce98aa
1183029827 <74184710+1183029827@users.noreply.github.com>
2024-04-14 12:35:55 +0800

    Update TrigCalculator.py

--- TrigCalculator.py
+++ TrigCalculator.py
@@ -1,7 +1,3 @@
-import tkinter as tk
-import math
-
-
 def calculate_factorial(n):
     """计算阶乘， 由宋纯豪同学进行编写"""
     return 0
@@ -27,22 +23,22 @@ def calculate_cos():
     return 0
 
 
-def calculate_tan_taylor(x, terms):
-    """使用泰勒级数展开计算正切函数， 由彭万同学进行编写"""
+def calculate_asin_taylor(x, terms):
+    """使用泰勒级数展开计算反正弦函数， 由彭万同学进行编写"""
     return 0
 
 
-def calculate_cot_taylor(x, terms):
-    """使用泰勒级数展开计算余切函数， 由彭万同学进行编写"""
+def calculate_atan_taylor(x, terms):
+    """使用泰勒级数展开计算反正切函数， 由彭万同学进行编写"""
     return 0
 
 
-def calculate_tan():
+def calculate_asin():
     """执行正切函数计算， 由彭万同学进行编写"""
     return 0
 
 
-def calculate_cot():
+def calculate_atan():
     """执行余切函数计算， 由彭万同学进行编写"""
     return 0
 
