fe305abfdaad529c2cf64364b113d393488a3818
1183029827 <74184710+1183029827@users.noreply.github.com>
2024-04-14 12:36:12 +0800

    Update TC_test.py

--- TC_test.py
+++ TC_test.py
@@ -1,14 +1,13 @@
 import math
-from TrigCalculator import calculate_sin_taylor, calculate_cos_taylor, calculate_tan_taylor, calculate_cot_taylor
+from TrigCalculator import calculate_sin_taylor, calculate_cos_taylor, calculate_asin_taylor, calculate_atan_taylor
 
 
 # 该函数用作对四种三角函数计算函数的功能进行测试，由 王财成同学 进行设计和验证
 # 函数名：test_trig_calculator，  调用来自TrigCalculator.py 的三角函数计算函数
-# calculate_sin_taylor, calculate_cos_taylor, calculate_tan_taylor, calculate_cot_taylor
+# calculate_sin_taylor, calculate_cos_taylor, calculate_asin_taylor, calculate_atan_taylor
 def test_trig_calculator():
     """测试"""
     return 0
-
 
 
 # 执行测试
