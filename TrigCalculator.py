aa31484b480534d022a86dd30110b8d8f1da2503
pengwan <166316088+pengwan27@users.noreply.github.com>
2024-04-22 10:42:18 +0800

    Update TrigCalculator.py
    添加了asin函数计算代码

--- TrigCalculator.py
+++ TrigCalculator.py
@@ -27,23 +27,34 @@ def calculate_cos():
     return 0
 
 
-def calculate_tan_taylor(x, terms):
-    """使用泰勒级数展开计算正切函数， 由彭万同学进行编写"""
+def calculate_asin_taylor(x, terms):
+    """使用泰勒级数展开计算反正弦函数， 由彭万同学进行编写"""
+    asinx = x
+    numerator = x
+    denominator = 1
+    sign = 1
+
+    # Loop to calculate arcsine using Taylor series
+    for n in range(1, terms):
+        numerator *= (x * x)
+        denominator = (2 * n) * (2 * n + 1)
+        sign *= -1
+        asinx += (sign * numerator) / denominator
+    return math.degrees(asinx)  # Convert the result to degrees
+
+
+def calculate_atan_taylor(x, terms):
+    """使用泰勒级数展开计算反正切函数， 由彭万同学进行编写"""
     return 0
 
 
-def calculate_cot_taylor(x, terms):
-    """使用泰勒级数展开计算余切函数， 由彭万同学进行编写"""
+def calculate_asin():
+    """执行反正弦函数计算， 由彭万同学进行编写"""
     return 0
 
 
-def calculate_tan():
-    """执行正切函数计算， 由彭万同学进行编写"""
-    return 0
-
-
-def calculate_cot():
-    """执行余切函数计算， 由彭万同学进行编写"""
+def calculate_atan():
+    """执行反正切函数计算， 由彭万同学进行编写"""
     return 0
 
 
